00ccad903cfd19fbd7dfe21fb306d33104f20bcc
xuhuang <xuhuang@theapollobox.com.cn>
2019-10-24 19:33:33 +0800

    change bread

--- _plugins/drops/breadcrumb_item.rb
+++ _plugins/drops/breadcrumb_item.rb
@@ -1,28 +1,28 @@
 module Jekyll
-   module Drops
-     class BreadcrumbItem < Liquid::Drop
-       extend Forwardable
+  module Drops
+    class BreadcrumbItem < Liquid::Drop
+      extend Forwardable
 
-       def initialize(side)
-         @side = side
-       end
+      def initialize(side)
+        @side = side
+      end
 
-       def position
-         @side[:position]
-       end
+      def position
+        @side[:position]
+      end
 
-       def title
-         @side[:title]
-       end
+      def title
+        @side[:title]
+      end
 
-       def url
-         @side[:url]
-       end
+      def url
+        @side[:url]
+      end
 
-       def rootimage
-         @side[:root_image]
-       end
-
-     end
-   end
- end
+      def rootimage
+        @side[:root_image]
+      end
+     
+    end
+  end
+end
